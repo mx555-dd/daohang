6627b4274aa823e71a8678ab3a19dec09cb3603d
mx555-dd <2629145505@qq.com>
2020-12-06 00:08:45 +0800

    daohang

---
+++ MyApplication21/app/src/main/java/com/example/myapplication21/constant/ImageList.java
@@ -0,0 +1,16 @@
+package com.example.myapplication21.constant;
+
+import com.example.myapplication21.R;
+
+import java.util.ArrayList;
+
+public class ImageList {
+    public static ArrayList<Integer> getDefault() {
+        ArrayList<Integer> imageList = new ArrayList<Integer>();
+        imageList.add(R.drawable.banner_1);
+        imageList.add(R.drawable.banner_2);
+        imageList.add(R.drawable.banner_3);
+        imageList.add(R.drawable.bannner_4);
+        return imageList;
+    }
+}
